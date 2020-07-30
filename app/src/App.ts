a10396a23b747f5027de777bde5811609d93d1df
Hao Yang <yanghao.anderson@gmail.com>
2020-07-30 11:07:53 -0400

    fix test

--- app/src/App.ts
+++ app/src/App.ts
@@ -5,7 +5,7 @@ import LastMessageManager from './LastMessageManager'
 
 let HChannelId = '547540063584518144'
 const TestChannelId = '719346869611790376'
-const testing = true
+const testing = false
 
 class App {
   public run () {
